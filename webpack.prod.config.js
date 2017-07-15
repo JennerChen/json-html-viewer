4781c434c4e20993ff64a8835f16f709bced5545
zhang qing <zhangqing332@live.com>
2017-07-15 17:03:59 +0800

    change fold structure

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,19 +1,21 @@
+
+
 const path = require('path');
 const webpack = require('webpack');
 module.exports = {
 	entry: {
-		doc: ['babel-polyfill','./examples/index.js']
+		jsonHtmlViewer: ['babel-polyfill', './src/index.js'],
 	},
 	output: {
-		filename: '[name].js',
+		filename: '[name].min.js',
 		publicPath: "/js/",
-		path: path.resolve(__dirname, 'examples/js')
+		path: path.resolve(__dirname, 'dist/js')
 	},
-	watch: true,
-	devtool: "eval-source-map",
+	watch: false,
+	devtool: false,
 	plugins: [
 		new webpack.DefinePlugin({
-			"process.env.NODE_ENV": JSON.stringify("DEV"),
+			"process.env.NODE_ENV": JSON.stringify("production"),
 		}),
 		new webpack.optimize.CommonsChunkPlugin({
 			name: ["vendor", "manifest"],
@@ -21,6 +23,9 @@ module.exports = {
 				// this assumes your vendor imports exist in the node_modules directory, 把所有node_modules下的文件一律加入类库包
 				return module.context && module.context.indexOf('node_modules') !== -1;
 			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: false
 		})
 	],
 	module: {
